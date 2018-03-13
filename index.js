5c2e1ecf9abb5dc9588ad1075ade4c3d9116bb78
Christian Maniewski <chmanie@fastmail.com>
2018-03-13 22:23:37 +0100

    Use prettier instead of eslint

--- index.js
+++ index.js
@@ -1,191 +1,206 @@
 'use strict';
 
-var interceptor = require('./lib/interceptor');
+const interceptor = require('./lib/interceptor');
 
-function plugin (wdInstance, options) {
+function plugin(wdInstance, options) {
+  /**
+   * instance need to have addCommand method
+   */
+  if (typeof wdInstance.addCommand !== 'function') {
+    throw new Error(
+      "you can't use WebdriverAjax with this version of WebdriverIO"
+    );
+  }
 
-    /**
-     * instance need to have addCommand method
-     */
-    if (typeof wdInstance.addCommand !== 'function') {
-        throw new Error('you can\'t use WebdriverAjax with this version of WebdriverIO');
+  wdInstance.addCommand('setupInterceptor', setup.bind(wdInstance));
+  wdInstance.addCommand('expectRequest', expectRequest.bind(wdInstance));
+  wdInstance.addCommand('assertRequests', assertRequests.bind(wdInstance));
+  wdInstance.addCommand('getRequest', getRequest.bind(wdInstance));
+  wdInstance.addCommand('getRequests', getRequest.bind(wdInstance));
+
+  function setup() {
+    wdInstance.__wdajaxExpectations = [];
+    return wdInstance.executeAsync(interceptor.setup);
+  }
+
+  function expectRequest(method, url, statusCode) {
+    wdInstance.__wdajaxExpectations.push({
+      method: method.toUpperCase(),
+      url: url,
+      statusCode: statusCode
+    });
+    return {};
+  }
+
+  function assertRequests() {
+    const expectations = wdInstance.__wdajaxExpectations;
+
+    if (!expectations.length) {
+      return Promise.reject(
+        new Error('No expectations found. Call .expectRequest() first')
+      );
     }
+    return getRequest().then(requests => {
+      if (expectations.length !== requests.length) {
+        return Promise.reject(
+          new Error(
+            'Expected ' +
+              expectations.length +
+              ' requests but was ' +
+              requests.length
+          )
+        );
+      }
 
-    wdInstance.addCommand('setupInterceptor', setup.bind(wdInstance));
-    wdInstance.addCommand('expectRequest', expectRequest.bind(wdInstance));
-    wdInstance.addCommand('assertRequests', assertRequests.bind(wdInstance));
-    wdInstance.addCommand('getRequest', getRequest.bind(wdInstance));
-    wdInstance.addCommand('getRequests', getRequest.bind(wdInstance));
+      for (let i = 0; i < expectations.length; i++) {
+        const ex = expectations[i];
+        const request = requests[i];
 
-    function setup () {
-        wdInstance.__wdajaxExpectations = [];
-        return wdInstance.executeAsync(interceptor.setup);
-    }
-
-    function expectRequest (method, url, statusCode) {
-        wdInstance.__wdajaxExpectations.push({
-            method: method.toUpperCase(),
-            url: url,
-            statusCode: statusCode
-        });
-        return {};
-    }
-
-    function assertRequests () {
-
-        var expectations = wdInstance.__wdajaxExpectations;
-
-        if (!expectations.length) {
-            return Promise.reject(new Error(
-                'No expectations found. Call .expectRequest() first'
-            ));
-        }
-        return getRequest().then(function assertAllRequests (requests) {
-
-            if (expectations.length !== requests.length) {
-                return Promise.reject(new Error(
-                    'Expected ' +
-                    expectations.length +
-                    ' requests but was ' +
-                    requests.length
-                ));
-            }
-
-            for (var i = 0; i < expectations.length; i++) {
-                var ex = expectations[i];
-                var request = requests[i];
-
-                if (request.method !== ex.method) {
-                    return Promise.reject(new Error(
-                        'Expected request to URL ' +
-                        request.url +
-                        ' to have method ' +
-                        ex.method +
-                        ' but was ' + request.method
-                    ));
-                }
-
-                if (ex.url instanceof RegExp && request.url && !request.url.match(ex.url)) {
-                    return Promise.reject(new Error(
-                        'Expected request ' +
-                        i +
-                        ' to match '
-                        + ex.url.toString() +
-                        ' but was ' +
-                        request.url
-                    ));
-                }
-
-                if (typeof ex.url == 'string' && request.url !== ex.url) {
-                    return Promise.reject(new Error(
-                        'Expected request ' +
-                        i +
-                        ' to have URL '
-                        + ex.url +
-                        ' but was ' +
-                        request.url
-                    ));
-                }
-
-                if (request.response.statusCode !== ex.statusCode) {
-                    return Promise.reject(new Error(
-                        'Expected request to URL ' +
-                        request.url +
-                        ' to have status ' +
-                        ex.statusCode +
-                        ' but was ' +
-                        request.response.statusCode
-                    ));
-                }
-
-            }
-
-            return wdInstance;
-
-        });
-
-    }
-
-    function getRequest (index) {
-        return wdInstance.execute(interceptor.getRequest, index)
-            .then(function (request) {
-                if (!request.value) {
-                    if (index != null) {
-                        return Promise.reject(new Error('Could not find request with index ' + index));
-                    }
-                    return [];
-                }
-                if (Array.isArray(request.value)) {
-                    return request.value.map(transformRequest);
-                }
-                // The edge driver does not seem to typecast arrays correctly
-                if (typeof request.value[0] == 'object') {
-                    return mapIndexed(request.value, transformRequest);
-                }
-                return transformRequest(request.value);
-            });
-    }
-
-    function transformRequest (req) {
-        if (!req) {
-            return;
+        if (request.method !== ex.method) {
+          return Promise.reject(
+            new Error(
+              'Expected request to URL ' +
+                request.url +
+                ' to have method ' +
+                ex.method +
+                ' but was ' +
+                request.method
+            )
+          );
         }
 
-        return {
-            url: req.url,
-            method: req.method && req.method.toUpperCase(),
-            body: parseBody(req.requestBody),
-            headers: normalizeRequestHeaders(req.requestHeaders),
-            response: {
-                headers: parseResponseHeaders(req.headers),
-                body: parseBody(req.body),
-                statusCode: req.statusCode
-            }
-        };
-    }
-
-    function normalizeRequestHeaders (headers) {
-        var normalized = {};
-        Object.keys(headers).forEach(function (key) {
-            normalized[key.toLowerCase()] = headers[key];
-        });
-        return normalized;
-    }
-
-    function parseResponseHeaders (str) {
-        var headers = {};
-        var arr = str.trim().replace(/\r/g, '').split('\n');
-        arr.forEach(function (header) {
-            var match = header.match(/^(.+)?:\s?(.+)$/);
-            if (match) {
-                headers[match[1].toLowerCase()] = match[2];
-            }
-        });
-        return headers;
-    }
-
-    function parseBody (str) {
-        var body;
-        try {
-            body = JSON.parse(str);
-        } catch (e) {
-            body = str;
+        if (
+          ex.url instanceof RegExp &&
+          request.url &&
+          !request.url.match(ex.url)
+        ) {
+          return Promise.reject(
+            new Error(
+              'Expected request ' +
+                i +
+                ' to match ' +
+                ex.url.toString() +
+                ' but was ' +
+                request.url
+            )
+          );
         }
-        return body;
+
+        if (typeof ex.url == 'string' && request.url !== ex.url) {
+          return Promise.reject(
+            new Error(
+              'Expected request ' +
+                i +
+                ' to have URL ' +
+                ex.url +
+                ' but was ' +
+                request.url
+            )
+          );
+        }
+
+        if (request.response.statusCode !== ex.statusCode) {
+          return Promise.reject(
+            new Error(
+              'Expected request to URL ' +
+                request.url +
+                ' to have status ' +
+                ex.statusCode +
+                ' but was ' +
+                request.response.statusCode
+            )
+          );
+        }
+      }
+
+      return wdInstance;
+    });
+  }
+
+  function getRequest(index) {
+    return wdInstance.execute(interceptor.getRequest, index).then(request => {
+      if (!request.value) {
+        if (index != null) {
+          return Promise.reject(
+            new Error('Could not find request with index ' + index)
+          );
+        }
+        return [];
+      }
+      if (Array.isArray(request.value)) {
+        return request.value.map(transformRequest);
+      }
+      // The edge driver does not seem to typecast arrays correctly
+      if (typeof request.value[0] == 'object') {
+        return mapIndexed(request.value, transformRequest);
+      }
+      return transformRequest(request.value);
+    });
+  }
+
+  function transformRequest(req) {
+    if (!req) {
+      return;
     }
 
-    // maps an 'array-like' object. returns proper array
-    function mapIndexed (obj, fn) {
-        var arr = [];
-        var max = Math.max.apply(Math, Object.keys(obj).map(Number));
-        for (var i = 0; i <= max; i++) {
-            arr.push(fn(obj[i], i));
-        }
-        return arr;
+    return {
+      url: req.url,
+      method: req.method && req.method.toUpperCase(),
+      body: parseBody(req.requestBody),
+      headers: normalizeRequestHeaders(req.requestHeaders),
+      response: {
+        headers: parseResponseHeaders(req.headers),
+        body: parseBody(req.body),
+        statusCode: req.statusCode
+      }
+    };
+  }
+
+  function normalizeRequestHeaders(headers) {
+    const normalized = {};
+    Object.keys(headers).forEach(key => {
+      normalized[key.toLowerCase()] = headers[key];
+    });
+    return normalized;
+  }
+
+  function parseResponseHeaders(str) {
+    const headers = {};
+    const arr = str
+      .trim()
+      .replace(/\r/g, '')
+      .split('\n');
+    arr.forEach(header => {
+      const match = header.match(/^(.+)?:\s?(.+)$/);
+      if (match) {
+        headers[match[1].toLowerCase()] = match[2];
+      }
+    });
+    return headers;
+  }
+
+  function parseBody(str) {
+    let body;
+    try {
+      body = JSON.parse(str);
+    } catch (e) {
+      body = str;
     }
+    return body;
+  }
+
+  // maps an 'array-like' object. returns proper array
+  function mapIndexed(obj, fn) {
+    const arr = [];
+    const max = Math.max.apply(Math, Object.keys(obj).map(Number));
+    for (let i = 0; i <= max; i++) {
+      arr.push(fn(obj[i], i));
+    }
+    return arr;
+  }
 }
 
 /**
  * expose WebdriverAjax
  */
-module.exports.init = plugin;
+exports.init = plugin;
--- test/spec/plugin_test.js
+++ test/spec/plugin_test.js
@@ -1,188 +1,188 @@
 'use strict';
 
-var assert = require('assert');
+const assert = require('assert');
 
-describe('webdriverajax', function () {
+describe('webdriverajax', function testSuite() {
+  this.timeout(process.env.CI ? 100000 : 10000);
 
-    this.timeout(process.env.CI ? 100000 : 10000);
+  const wait = process.env.CI ? 10000 : 1000;
 
-    var wait = process.env.CI ? 10000 : 1000;
-
-    it('sets up the interceptor', function () {
-        assert.equal(typeof browser.setupInterceptor, 'function');
-        browser.url('/get.html');
-        browser.setupInterceptor();
-        var ret = browser.execute(function checkSetup () {
-            return window.__webdriverajax;
-        });
-        assert.deepEqual(ret.value, { requests: [] });
+  it('sets up the interceptor', () => {
+    assert.equal(typeof browser.setupInterceptor, 'function');
+    browser.url('/get.html');
+    browser.setupInterceptor();
+    const ret = browser.execute(() => {
+      return window.__webdriverajax;
     });
+    assert.deepEqual(ret.value, { requests: [] });
+  });
 
-    it('can intercept a simple GET request', function () {
-        browser.url('/get.html').setupInterceptor();
-        browser.expectRequest('GET', '/get.json', 200);
-        browser.click('#button').pause(wait);
-        browser.assertRequests();
+  it('can intercept a simple GET request', () => {
+    browser.url('/get.html').setupInterceptor();
+    browser.expectRequest('GET', '/get.json', 200);
+    browser.click('#button').pause(wait);
+    browser.assertRequests();
+  });
+
+  it('can use regular expressions for urls', () => {
+    browser.url('/get.html').setupInterceptor();
+    browser.expectRequest('GET', /get\.json/, 200);
+    browser.click('#button').pause(wait);
+    browser.assertRequests();
+  });
+
+  it('errors on wrong request count', () => {
+    browser.url('/get.html').setupInterceptor();
+    browser
+      .expectRequest('GET', '/get.json', 200)
+      .expectRequest('GET', '/get.json', 200);
+    browser.click('#button').pause(wait);
+    assert.throws(() => {
+      browser.assertRequests();
+    }, /Expected/);
+  });
+
+  it('errors on wrong method', () => {
+    browser.url('/get.html').setupInterceptor();
+    browser.expectRequest('PUT', '/get.json', 200);
+    browser.click('#button').pause(wait);
+    assert.throws(() => {
+      browser.assertRequests();
+    }, /PUT/);
+  });
+
+  it('errors on wrong URL', () => {
+    browser.url('/get.html').setupInterceptor();
+    browser.expectRequest('GET', '/wrong.json', 200);
+    browser.click('#button').pause(wait);
+    assert.throws(() => {
+      browser.assertRequests();
+    }, /wrong\.json/);
+  });
+
+  it("errors if regex doesn't match URL", () => {
+    browser.url('/get.html').setupInterceptor();
+    browser.expectRequest('GET', /wrong\.json/, 200);
+    browser.click('#button').pause(wait);
+    assert.throws(() => {
+      browser.assertRequests();
+    }, /get\.json/);
+  });
+
+  it('errors on wrong status code', () => {
+    browser.url('/get.html').setupInterceptor();
+    browser.expectRequest('GET', '/get.json', 404);
+    browser.click('#button').pause(wait);
+    assert.throws(() => {
+      browser.assertRequests();
+    }, /404/);
+  });
+
+  it('can access a certain request', () => {
+    browser.url('/get.html').setupInterceptor();
+    browser.click('#button').pause(wait);
+    const request = browser.getRequest(0);
+    assert.equal(request.method, 'GET');
+    assert.equal(request.url, '/get.json');
+    assert.deepEqual(request.response.body, { OK: true });
+    assert.equal(request.response.statusCode, 200);
+    assert.equal(request.response.headers['content-length'], '15');
+  });
+
+  it('can get multiple requests', () => {
+    browser.url('/get.html').setupInterceptor();
+    browser.click('#button').pause(wait);
+    browser.click('#button').pause(wait);
+    const requests = browser.getRequests();
+    assert(Array.isArray(requests));
+    assert.equal(requests.length, 2);
+    assert.equal(requests[0].method, 'GET');
+    assert.equal(requests[1].method, 'GET');
+  });
+
+  it('survives page changes', () => {
+    browser.url('/page_change.html').setupInterceptor();
+    browser.click('#button1').pause(wait);
+    const requests = browser.getRequests();
+    assert(Array.isArray(requests));
+    assert.equal(requests.length, 1);
+    assert.equal(requests[0].method, 'GET');
+  });
+
+  it('survives page changes using multiple requests', () => {
+    browser.url('/page_change.html').setupInterceptor();
+    browser
+      .click('#button1')
+      .click('#button2')
+      .pause(wait);
+    const requests = browser.getRequests();
+    assert(Array.isArray(requests));
+    assert.equal(requests.length, 2);
+    assert.equal(requests[0].method, 'GET');
+    assert.equal(requests[1].method, 'GET');
+  });
+
+  it('can assess the request body using string data', () => {
+    browser.url('/post.html').setupInterceptor();
+    browser.click('#buttonstring').pause(wait);
+    const request = browser.getRequest(0);
+    assert.equal(request.body, 'foobar');
+  });
+
+  it('can assess the request body using JSON data', () => {
+    browser.url('/post.html').setupInterceptor();
+    browser.click('#buttonjson').pause(wait);
+    const request = browser.getRequest(0);
+    assert.deepEqual(request.body, { foo: 'bar' });
+  });
+
+  it('can assess the request body using form data', () => {
+    browser.url('/post.html').setupInterceptor();
+    browser.click('#buttonform').pause(wait);
+    const request = browser.getRequest(0);
+    assert.deepEqual(request.body, { foo: ['bar'] });
+  });
+
+  it('can assess the request body using JSON data', () => {
+    browser.url('/post.html').setupInterceptor();
+    browser.click('#buttonjson').pause(wait);
+    const request = browser.getRequest(0);
+    assert.equal(request.headers['content-type'], 'application/json');
+  });
+
+  it('can get initialised inside an iframe', () => {
+    browser.url('/frame.html').setupInterceptor();
+    const ret = browser.execute(function checkSetup() {
+      return window.__webdriverajax;
     });
-
-    it('can use regular expressions for urls', function () {
-        browser.url('/get.html').setupInterceptor();
-        browser.expectRequest('GET', /get\.json/, 200);
-        browser.click('#button').pause(wait);
-        browser.assertRequests();
+    assert.deepEqual(ret.value, { requests: [] });
+    browser.waitForExist('#getinframe');
+    const frame = browser.element('#getinframe');
+    browser.frame(frame.value);
+    browser.setupInterceptor();
+    const frameRet = browser.execute(function checkSetup() {
+      return window.__webdriverajax;
     });
-
-    it('errors on wrong request count', function () {
-        browser.url('/get.html').setupInterceptor();
-        browser
-            .expectRequest('GET', '/get.json', 200)
-            .expectRequest('GET', '/get.json', 200);
-        browser.click('#button').pause(wait);
-        assert.throws(function () {
-            browser.assertRequests();
-        }, /Expected/);
+    assert.deepEqual(frameRet.value, { requests: [] });
+    browser.expectRequest('GET', '/get.json', 200);
+    browser.click('#button').pause(wait);
+    browser.assertRequests();
+    browser.frameParent();
+    assert.throws(() => {
+      browser.assertRequests();
     });
+  });
 
-    it('errors on wrong method', function () {
-        browser.url('/get.html').setupInterceptor();
-        browser.expectRequest('PUT', '/get.json', 200);
-        browser.click('#button').pause(wait);
-        assert.throws(function () {
-            browser.assertRequests();
-        }, /PUT/);
-    });
+  it('errors with no requests set up', () => {
+    browser.url('/get.html').setupInterceptor();
+    assert.throws(() => {
+      browser.assertRequests();
+    }, /No\sexpectations\sfound/);
+  });
 
-    it('errors on wrong URL', function () {
-        browser.url('/get.html').setupInterceptor();
-        browser.expectRequest('GET', '/wrong.json', 200);
-        browser.click('#button').pause(wait);
-        assert.throws(function () {
-            browser.assertRequests();
-        }, /wrong\.json/);
-    });
-
-    it('errors if regex doesn\'t match URL', function () {
-        browser.url('/get.html').setupInterceptor();
-        browser.expectRequest('GET', /wrong\.json/, 200);
-        browser.click('#button').pause(wait);
-        assert.throws(function () {
-            browser.assertRequests();
-        }, /get\.json/);
-
-    });
-
-    it('errors on wrong status code', function () {
-        browser.url('/get.html').setupInterceptor();
-        browser.expectRequest('GET', '/get.json', 404);
-        browser.click('#button').pause(wait);
-        assert.throws(function () {
-            browser.assertRequests();
-        }, /404/);
-    });
-
-    it('can access a certain request', function () {
-        browser.url('/get.html').setupInterceptor();
-        browser.click('#button').pause(wait);
-        var request = browser.getRequest(0);
-        assert.equal(request.method, 'GET');
-        assert.equal(request.url, '/get.json');
-        assert.deepEqual(request.response.body, { OK: true });
-        assert.equal(request.response.statusCode, 200);
-        assert.equal(request.response.headers['content-length'], '15');
-    });
-
-    it('can get multiple requests', function () {
-        browser.url('/get.html').setupInterceptor();
-        browser.click('#button').pause(wait);
-        browser.click('#button').pause(wait);
-        var requests = browser.getRequests();
-        assert(Array.isArray(requests));
-        assert.equal(requests.length, 2);
-        assert.equal(requests[0].method, 'GET');
-        assert.equal(requests[1].method, 'GET');
-    });
-
-    it('survives page changes', function () {
-        browser.url('/page_change.html').setupInterceptor();
-        browser.click('#button1').pause(wait);
-        var requests = browser.getRequests();
-        assert(Array.isArray(requests));
-        assert.equal(requests.length, 1);
-        assert.equal(requests[0].method, 'GET');
-    });
-
-    it('survives page changes using multiple requests', function () {
-        browser.url('/page_change.html').setupInterceptor();
-        browser.click('#button1').click('#button2').pause(wait);
-        var requests = browser.getRequests();
-        assert(Array.isArray(requests));
-        assert.equal(requests.length, 2);
-        assert.equal(requests[0].method, 'GET');
-        assert.equal(requests[1].method, 'GET');
-    });
-
-    it('can assess the request body using string data', function () {
-        browser.url('/post.html').setupInterceptor();
-        browser.click('#buttonstring').pause(wait);
-        var request = browser.getRequest(0);
-        assert.equal(request.body, 'foobar');
-    });
-
-    it('can assess the request body using JSON data', function () {
-        browser.url('/post.html').setupInterceptor();
-        browser.click('#buttonjson').pause(wait);
-        var request = browser.getRequest(0);
-        assert.deepEqual(request.body, { foo: 'bar' });
-    });
-
-    it('can assess the request body using form data', function () {
-        browser.url('/post.html').setupInterceptor();
-        browser.click('#buttonform').pause(wait);
-        var request = browser.getRequest(0);
-        assert.deepEqual(request.body, { foo: ['bar'] });
-    });
-
-    it('can assess the request body using JSON data', function () {
-        browser.url('/post.html').setupInterceptor();
-        browser.click('#buttonjson').pause(wait);
-        var request = browser.getRequest(0);
-        assert.equal(request.headers['content-type'], 'application/json');
-    });
-
-    it('can get initialised inside an iframe', function () {
-        browser.url('/frame.html').setupInterceptor();
-        var ret = browser.execute(function checkSetup () {
-            return window.__webdriverajax;
-        });
-        assert.deepEqual(ret.value, { requests: [] });
-        browser.waitForExist('#getinframe');
-        var frame = browser.element('#getinframe');
-        browser.frame(frame.value);
-        browser.setupInterceptor();
-        var frameRet = browser.execute(function checkSetup () {
-            return window.__webdriverajax;
-        });
-        assert.deepEqual(frameRet.value, { requests: [] });
-        browser.expectRequest('GET', '/get.json', 200);
-        browser.click('#button').pause(wait);
-        browser.assertRequests();
-        browser.frameParent();
-        assert.throws(() => {
-            browser.assertRequests();
-        });
-    });
-
-    it('errors with no requests set up', function () {
-        browser.url('/get.html').setupInterceptor();
-        assert.throws(() => {
-            browser.assertRequests();
-        }, /No\sexpectations\sfound/);
-    });
-
-    it('returns an empty array for no captured requests', function () {
-        browser.url('/get.html').setupInterceptor();
-        var count = browser.getRequests();
-        assert.deepEqual(count, []);
-    });
-
+  it('returns an empty array for no captured requests', () => {
+    browser.url('/get.html').setupInterceptor();
+    const count = browser.getRequests();
+    assert.deepEqual(count, []);
+  });
 });
