65bc776a5a5b51851a938cc19e773cd8c6985865
Christian Maniewski <code@chmanie.com>
2020-04-29 13:47:49 +0200

    Reformat code to simpler rules

--- test/utils.js
+++ test/utils.js
@@ -1,67 +1,67 @@
-'use strict'
+'use strict';
 
-const http = require('http')
+const http = require('http');
 
-const selenium = require('selenium-standalone')
-const nodeStatic = require('node-static')
+const selenium = require('selenium-standalone');
+const nodeStatic = require('node-static');
 
-let grid, staticServer
+let grid, staticServer;
 
 module.exports = {
   startStaticServer,
   stopStaticServer,
   startSelenium,
-  stopSelenium,
-}
+  stopSelenium
+};
 
 function startStaticServer() {
   return new Promise((resolve, reject) => {
-    const file = new nodeStatic.Server('./test/site')
+    const file = new nodeStatic.Server('./test/site');
     const server = http
       .createServer((request, response) => {
         request
           .addListener('end', () => {
-            file.serve(request, response)
+            file.serve(request, response);
           })
-          .resume()
+          .resume();
       })
       .listen(8080, err => {
         if (err) {
-          return reject(err)
+          return reject(err);
         }
-        console.log('Started static server on port ' + 8080)
-        staticServer = server
-        resolve(server)
-      })
-  })
+        console.log('Started static server on port ' + 8080);
+        staticServer = server;
+        resolve(server);
+      });
+  });
 }
 
 function startSelenium() {
   return new Promise((resolve, reject) => {
     selenium.start((err, sel) => {
       if (err) {
-        return reject(err)
+        return reject(err);
       }
-      console.log('Started Selenium server')
-      grid = sel
-      resolve(sel)
-    })
-  })
+      console.log('Started Selenium server');
+      grid = sel;
+      resolve(sel);
+    });
+  });
 }
 
 function stopSelenium() {
   return new Promise((resolve, reject) => {
     grid.on('exit', () => {
-      resolve(true)
-    })
-    grid.kill()
-  })
+      resolve(true);
+    });
+    grid.kill();
+  });
 }
 
 function stopStaticServer() {
   return new Promise((resolve, reject) => {
     staticServer.close(() => {
-      resolve(true)
-    })
-  })
+      resolve(true);
+    });
+  });
 }
